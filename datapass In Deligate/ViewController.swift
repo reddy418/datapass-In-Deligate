124e8be934187aa53bb80572c029abadd3382aa3
THOTA NAGARAJU <naga.thota99@achiever-122.local>
2019-11-18 12:54:29 +0530

    Initial Commit

---
+++ datapass In Deligate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  datapass In Deligate
+//
+//  Created by THOTA NAGARAJU on 11/18/19.
+//  Copyright © 2019 THOTA NAGARAJU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
